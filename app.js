819901fe2717bbde0d8397ffd976ce0fdf840fe5
Theo Snyman <codedbytheo@gmail.com>
2024-05-28 13:21:36 +0200

    Added terminal menu functionality
    The program is now working smoothly as intended.

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const searchTrack = async (trackName) => {
     }
   })
 
-  const firstTrack = searchRequest.data.tracks.item[0]
+  const firstTrack = searchRequest.data.tracks.items[0]
 
   if (firstTrack) {
     return {
@@ -45,3 +45,63 @@ const searchTrack = async (trackName) => {
     return null
   }
 }
+
+const askQuestion = async (question) => {
+  while (true) {
+    const answer = await new Promise((resolve) => {
+      readline.question(question, (value) => resolve(value))
+    })
+
+    
+
+    if (answer.trim() !== '') {
+      return answer
+    } else {
+      console.log('---------------------')
+      console.error('No input received... Try again.')
+      console.log('---------------------')
+    }
+  }
+}
+
+const mainMenu = async () => {
+  while (true) {
+    console.log('\nMenu:')
+    console.log('---------------------')
+    console.log(`1. Find Track`)
+    console.log(`2. Exit Application`)
+    console.log('---------------------')
+
+    const option = await askQuestion(`Option (1 / 2): `)
+
+    switch (option) {
+      case '1':
+        console.log('---------------------')
+        const songName = await askQuestion('Search Track: ')
+        const songDetails = await searchTrack(songName)
+        if (songDetails) {
+          console.log('---------------------')
+          console.log(`Artists: ${songDetails.artists}`)
+          console.log(`Song: ${songDetails.name}`)
+          console.log(`Preview Link: ${songDetails.previewURL ? songDetails.previewURL : "No preview available..."}`)
+          console.log(`Album: ${songDetails.album}`)
+          console.log(`Album Release Date: ${songDetails.albumRelease}`)
+          console.log('---------------------')
+        } else {
+          console.log('Song not found...')
+        }
+        break
+      case '2':
+        console.log('---------------------')
+        console.log('Exiting the application...')
+        readline.close()
+        return
+      default:
+        console.log('---------------------')
+        console.log('Invalid option selected... Try again.')
+        console.log('---------------------')
+    }
+  }
+}
+
+mainMenu()
